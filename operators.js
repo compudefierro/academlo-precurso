0a03b4fc65e03f261d1e0d61de12535f9defef46
compudefierro <jorgeeldri@gmail.com>
2023-05-23 19:44:32 -0300

    Operadores de comparacion

---
+++ operators.js
@@ -0,0 +1,12 @@
+"use strict";
+// Casi Habilitado.
+let edad = 42;
+let esMayor = edad >= 18;
+let habilitado = esMayor ? "Si" : "No";
+console.log("Habilitador para el curso: " + habilitado);
+
+// Caso Inhabilitado.
+edad = 17;
+esMayor = edad >= 18;
+habilitado = esMayor ? "Si" : "No";
+console.log("Habilitador para el curso: " + habilitado);
