f488325572a4d5524aed90bee448476eb9a4fc21
compudefierro <jorgeeldri@gmail.com>
2023-05-23 20:06:45 -0300

    Metodos para strings

---
+++ strings-methods.js
@@ -0,0 +1,14 @@
+"use strict";
+let nombre = "Jorge";
+/*
+endswith()
+lastIndexOf()
+slice()
+replace()
+includes()
+*/
+console.log(nombre.endsWith("e"));
+console.log(nombre.lastIndexOf("o"));
+console.log(nombre.slice(0, 3));
+console.log(nombre.replace("e", "ito"));
+console.log(nombre.includes("J"));
